4971acdb5d1f9484a59cef4a1230636e22f612d8
Rich Garcia <rchgrca@yahoo.com>
2017-08-14 11:30:10 -0700

    fixes lint errors

--- src/routes/Exercise/components/Nemo.js
+++ src/routes/Exercise/components/Nemo.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import NemoImage from '../assets/nemo.png'
-import '../../Home/components/HomeView.scss'
+import NemoImg from '../assets/nemo.png'
+import './ExerciseView.scss'
 
 export const Nemo = ({ counter }) => (
-  <img className='duck' style={{ position: 'absolute', top: `${counter.y}px`, left: `${counter.x}px` }} src={NemoImage} />
+  <img className='nemo' style={{ top: `${counter.y}px`, left: `${counter.x}px` }} src={NemoImg} />
 )
+
+Nemo.propTypes = {
+  counter: PropTypes.object
+}
+
 export default Nemo
