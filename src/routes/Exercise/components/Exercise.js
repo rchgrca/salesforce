b589e9965c8e4baacf7a085e927fa26a8f87490c
Rich Garcia <rchgrca@yahoo.com>
2017-08-14 11:38:55 -0700

    fixes lint errors

--- src/routes/Exercise/components/Exercise.js
+++ src/routes/Exercise/components/Exercise.js
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types'
 import Duck from './Duck'
 import Nemo from './Nemo'
 
-export const Exercise = ({ counter = {x:0, y:0}, moveNemo }) => (
+export const Exercise = ({ counter = { x:0, y:0 }, moveNemo }) => (
   <div style={{ margin: '0 auto' }} >
     <h2>Exercise:</h2>
-    <input type="text" onKeyDown={moveNemo}/>
-    <div className='waterContainer' style={{ position: 'relative', height: '500px'}}>
-      <Nemo counter={counter}/>
-      <Duck/>
+    <input type='text' onKeyDown={moveNemo} />
+    <div className='waterContainer' style={{ position: 'relative', height: '500px' }} >
+      <Nemo counter={counter} />
+      <Duck />
     </div>
   </div>
 )
 
 Exercise.propTypes = {
-  coordinate: PropTypes.object
+  counter: PropTypes.object,
+  moveNemo: PropTypes.func,
 }
 
 export default Exercise
