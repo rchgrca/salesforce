1845bc7f182ec4ad7d98f638c6f2bd039e1e0ae4
Rich Garcia <rchgrca@yahoo.com>
2017-08-14 10:58:37 -0700

    updates left position

--- src/routes/Exercise/components/Duck.js
+++ src/routes/Exercise/components/Duck.js
@@ -4,6 +4,6 @@ import DuckImage from '../../Home/assets/Duck.jpg'
 import '../../Home/components/HomeView.scss'
 
 export const Duck = ({ alt }) => (
-  <img alt='This is a duck component!' className='duck' style={{ position: 'absolute' }} src={DuckImage} />
+  <img alt='This is a duck component!' className='duck' style={{ position: 'absolute', left: '50%' }} src={DuckImage} />
 )
 export default Duck
