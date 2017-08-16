6ac2ce7232856fe08d33f8e0d4db48c9ac5d4f77
Rich Garcia <rchgrca@yahoo.com>
2017-08-16 08:22:11 -0700

    renames class to Dori and other left coord changes

--- src/routes/Exercise/components/Dori.js
+++ src/routes/Exercise/components/Dori.js
@@ -2,10 +2,10 @@ import React from 'react'
 import DoriImage from '../assets/dori.png'
 import './ExerciseView.scss'
 
-export const Duck = ({ counter }) => {
-  const x = counter.hasCollided ? 300 : 200
+export const Dori = ({ counter: { x, y, hasCollided } }) => {
+  const xCoord = hasCollided ? x + 300 : x
   return (
-    <img className='dori' style={{ left: `${x}px` }} src={DoriImage} />
+    <img className='dori' style={{ left: `${xCoord}px` }} src={DoriImage} />
   )
 }
-export default Duck
+export default Dori
